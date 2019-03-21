c8102800ecb00c5f7e7f55db23eafb40a8dc89d7
mninozebra <mni@nozebra.dk>
2019-03-21 17:39:45 +0100

    Added some new content

--- pages/index.vue
+++ pages/index.vue
@@ -33,7 +33,7 @@
         <div class="section">
           <b-container fluid>
             <b-row>
-              <b-col cols="4" class="m-auto p-5">
+              <b-col cols="4" class="m-md-auto p-md-5">
                 <h1>Evner</h1>
                 <p>Mit navn er Mikkel Hornbech Nielsen, jeg er 26 år gammel og født og opvokset i smilets by. Jeg har en kæmpe passion for hjemmesideudvikling og grafisk design. Udover min passion elsker jeg fodbold, familie, venner, gaming og kultur. Jeg har en stor IT-viden, og er aldrig bleg for en udfordring og meget interesseret i alt nyt indenfor mit fag.</p>
                 <b-button variant="secondary">Primary</b-button>
@@ -43,16 +43,62 @@
           </b-container>
         </div>
         <div class="section">
-          <h1>Projekter</h1>
-          <div class="slide">
-            <p>Slide 2.1</p>
-          </div>
-          <div class="slide">
-            <p>Slide 2.2</p>
-          </div>
-          <div class="slide">
-            <p>Slide 2.3</p>
-          </div>
+          <b-container fluid>
+            <b-row>
+              <b-col cols="12" class="p-5">
+                <h1>Projekter</h1>
+              </b-col>
+            </b-row>
+
+            <div class="slide beeroclock-slide">
+              <b-row>
+                <b-col cols="6" class="p-5">
+                  <h1>BeerOClock</h1>
+                  <p>Mit navn er Mikkel Hornbech Nielsen, jeg er 26 år gammel og født og opvokset i smilets by. Jeg har en kæmpe passion for hjemmesideudvikling og grafisk design. Udover min passion elsker jeg fodbold, familie, venner, gaming og kultur. Jeg har en stor IT-viden, og er aldrig bleg for en udfordring og meget interesseret i alt nyt indenfor mit fag.</p>
+                  <b-button variant="secondary">Primary</b-button>
+                </b-col>
+                <b-col md="6" cols="6" class="p-5">
+                  <parallax-container>
+                    <parallax-element :parallaxStrength="1" :type="'translation'">
+                      <b-img-lazy fluid src="/img/beeroclock.png" alt="Mikkel Hornbech Nielsen"/>
+                    </parallax-element>
+                  </parallax-container>
+                </b-col>
+              </b-row>
+            </div>
+            <div class="slide">
+              <b-row>
+                <b-col cols="6" class="p-5">
+                  <h1>BeerOClock</h1>
+                  <p>Mit navn er Mikkel Hornbech Nielsen, jeg er 26 år gammel og født og opvokset i smilets by. Jeg har en kæmpe passion for hjemmesideudvikling og grafisk design. Udover min passion elsker jeg fodbold, familie, venner, gaming og kultur. Jeg har en stor IT-viden, og er aldrig bleg for en udfordring og meget interesseret i alt nyt indenfor mit fag.</p>
+                  <b-button variant="secondary">Primary</b-button>
+                </b-col>
+                <b-col md="6" cols="6" class="p-5">
+                  <parallax-container>
+                    <parallax-element :parallaxStrength="1" :type="'translation'">
+                      <b-img-lazy fluid src="/img/beeroclock.png" alt="Mikkel Hornbech Nielsen"/>
+                    </parallax-element>
+                  </parallax-container>
+                </b-col>
+              </b-row>
+            </div>
+            <div class="slide">
+              <b-row>
+                <b-col cols="4" class="p-5">
+                  <h1>BeerOClock</h1>
+                  <p>Mit navn er Mikkel Hornbech Nielsen, jeg er 26 år gammel og født og opvokset i smilets by. Jeg har en kæmpe passion for hjemmesideudvikling og grafisk design. Udover min passion elsker jeg fodbold, familie, venner, gaming og kultur. Jeg har en stor IT-viden, og er aldrig bleg for en udfordring og meget interesseret i alt nyt indenfor mit fag.</p>
+                  <b-button variant="secondary">Primary</b-button>
+                </b-col>
+                <b-col md="8" cols="6" class="p-5">
+                  <parallax-container>
+                    <parallax-element :parallaxStrength="1" :type="'translation'">
+                      <b-img-lazy fluid src="/img/beeroclock.png" alt="Mikkel Hornbech Nielsen"/>
+                    </parallax-element>
+                  </parallax-container>
+                </b-col>
+              </b-row>
+            </div>
+          </b-container>
         </div>
         <div class="section">
           <h1>Kontakt</h1>
